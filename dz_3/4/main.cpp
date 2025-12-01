cb73818516c6c7b1f07f5080d3645ced210a45d1
Gosha-Gurin <gurin.george06@gmail.com>
2025-12-01 04:23:13 +0300

    3.4 problem completed.

--- dz_3/4/main.cpp
+++ dz_3/4/main.cpp
@@ -29,11 +29,11 @@ void PrintList(NodeList* Head){
 	NodeList* tmp = Head;
 
 	while (tmp != nullptr){
-		std::cout << tmp->value << "; Value of random ptr: ";
+		std::cout << tmp->value << " -> " << tmp << "; Value of random ptr: ";
 		if (tmp->rand == nullptr){
-			std::cout << "NULL" << std::endl;
+			std::cout << "NULL -> 0x0" << std::endl;
 		} else {
-			std::cout << tmp->rand->value << std::endl;
+			std::cout << tmp->rand->value << " -> " << tmp->rand << std::endl;
 		}
 		tmp = tmp->next;
 	}
@@ -63,7 +63,7 @@ NodeList* ReadList(){
 	NodeList* tmp2 = Head;
 
 	for (int i = 0; i < vec.size(); i++){
-		if (vec[i] == 0){
+		if (vec[i] <= 0){
 			tmp1->rand = nullptr;
 		} else {
 			for (int j = 0; j < vec[i]-1; j++){
@@ -81,13 +81,83 @@ NodeList* ReadList(){
 
 
 
+// NodeList* ListCopy(NodeList* Head){
+// 	NodeList* CopyHead = new NodeList(Head->value);
+
+// 	NodeList* tmp1 = CopyHead;
+// 	NodeList* tmp2 = Head->next;
+// 	NodeList* tmp3 = Head;
+// 	NodeList* tmp4 = Head;
+
+// 	while(tmp2 != nullptr){
+// 		NodeList* tmp = new NodeList(tmp2->value);
+// 		tmp1->next = tmp;
+// 		tmp1 = tmp;
+// 		tmp2 = tmp2->next;
+// 	}
+
+// 	tmp1 = CopyHead;
+// 	tmp2 = CopyHead;
+// 	tmp3 = Head;
+// 	tmp4 = Head;
+
+// 	// std::cout << "\n";
+// 	// PrintList(CopyHead);
+// 	// std::cout << "\n";
+// 	// PrintList(Head);
+
+// 	while(tmp1 != nullptr){
+
+// 		if(tmp4->rand == nullptr){
+// 			tmp1->rand = nullptr;
+
+// 			tmp1 = tmp1->next;
+// 			tmp2 = CopyHead;
+
+// 			tmp4 = tmp4->next;
+// 			tmp3 = Head;
+// 		} else{
+// 			while(tmp3 != tmp4->rand){
+// 				tmp2 = tmp2->next;
+// 				tmp3 = tmp3->next;
+// 			}
+
+// 			tmp1->rand = tmp2;
+
+// 			tmp1 = tmp1->next;
+// 			tmp2 = CopyHead;
+
+// 			tmp4 = tmp4->next;
+// 			tmp3 = Head;
+// 		}
+// 	}
+
+// 	PrintList(CopyHead);
+
+// 	return CopyHead;
+// }
+
 NodeList* ListCopy(NodeList* Head){
+	// NodeList* CopyTMP = new NodeList(Head->value);
+
+	// NodeList* tmp1 = CopyTMP;
+	// NodeList* tmp2 = Head->next;
+	// NodeList* tmp3 = Head;
+	// NodeList* tmp4 = Head;
+
+	// while(tmp2 != nullptr){
+	// 	NodeList* tmp = new NodeList(tmp2->value);
+	// 	tmp1->next = tmp2;
+	// 	tmp1 = tmp;
+	// 	tmp2 = tmp2->next;
+	// }
+
+	// PrintList(CopyTMP);
+
 	NodeList* CopyHead = new NodeList(Head->value);
 
 	NodeList* tmp1 = CopyHead;
 	NodeList* tmp2 = Head->next;
-	NodeList* tmp3 = Head;
-	NodeList* tmp4 = Head;
 
 	while(tmp2 != nullptr){
 		NodeList* tmp = new NodeList(tmp2->value);
@@ -96,43 +166,66 @@ NodeList* ListCopy(NodeList* Head){
 		tmp2 = tmp2->next;
 	}
 
-	tmp1 = CopyHead;
+	
+	tmp1 = Head;
 	tmp2 = CopyHead;
-	tmp3 = Head;
-	tmp4 = Head;
 
-	// std::cout << "\n";
-	// PrintList(CopyHead);
-	// std::cout << "\n";
-	// PrintList(Head);
+	NodeList* tmp3 = CopyHead->next;
 
-	while(tmp1 != nullptr){
+	//Сшивание Head и CopyHead
+	while(tmp1->next != nullptr){
+		tmp2->next = tmp1->next;
+		tmp1->next = tmp2;
 
-		if(tmp4->rand == nullptr){
-			tmp1->rand = nullptr;
 
-			tmp1 = tmp1->next;
-			tmp2 = CopyHead;
-
-			tmp4 = tmp4->next;
-			tmp3 = Head;
-		} else{
-			while(tmp3 != tmp4->rand){
-				tmp2 = tmp2->next;
-				tmp3 = tmp3->next;
-			}
-
-			tmp1->rand = tmp2;
-
-			tmp1 = tmp1->next;
-			tmp2 = CopyHead;
-
-			tmp4 = tmp4->next;
-			tmp3 = Head;
-		}
+		tmp1 = tmp2->next;
+		tmp2 = tmp3;
+		tmp3 = tmp2->next;
 	}
 
-	PrintList(CopyHead);
+	tmp1->next = tmp2;
+
+
+	tmp1 = Head;
+
+
+	//Задаем rand
+	while(tmp1 != nullptr){
+
+		if(tmp1->rand == nullptr){
+			tmp1->next->rand = nullptr;
+		} else{
+			tmp1->next->rand = tmp1->rand->next;
+		}
+
+		tmp1 = tmp1->next->next;
+	}
+
+	tmp1 = Head;
+	tmp2 = CopyHead;
+
+
+	//Обратная склейка
+	while(tmp2->next != nullptr){
+		// std::cout << tmp1->value << "\n";
+
+		tmp1->next = tmp2->next;
+		tmp2->next = tmp2->next->next;
+
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->next;
+	}
+
+	tmp1->next = nullptr;
+	tmp2->next = nullptr;
+
+	// std::cout << "\n";
+
+	// PrintList(Head);
+
+	// std::cout << "\n";
+
+	// PrintList(CopyHead);
 
 	return CopyHead;
 }
@@ -144,11 +237,18 @@ int main(){
 
 	NodeList* Head = ReadList();
 
-	ListCopy(Head);
+	NodeList* Copy = ListCopy(Head);
 
-	// PrintList(Head);
+	std::cout << "\n";
 
-	NodeList* Copy;
+	PrintList(Head);
+
+	std::cout << "\n";
+
+	PrintList(Copy);
+
+
+	// delete ;
 
 	return 0;
 }
